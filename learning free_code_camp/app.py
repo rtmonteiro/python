2c9455de813e9f236beed0820f9e86e5620d9324
Ryan Monteiro <37312658+rtmonteiro@users.noreply.github.com>
2018-11-23 19:41:29 -0200

    aprendendo variáveis

--- learning free_code_camp/app.py
+++ learning free_code_camp/app.py
@@ -1,4 +1,11 @@
-print("   /|")
-print("  / |")
-print(" /  |")
-print("/___|")
+character_name = "Marcelo"
+character_age = 40
+years_to_sum = 18
+value_sum = character_age + years_to_sum
+
+
+print("There once was a man named " + character_name + ", ")
+print("he was " + str(character_age) + " years old.")
+print("He really liked the name " + character_name + ", ")
+print("but didn't like being " + str(character_age) + ".")
+print("In " + str(years_to_sum) + " years, he will be " + str(value_sum))
