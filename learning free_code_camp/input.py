7ff46a64cfd6f8e3eab4d5ba97c373e45fd715a8
rtmonteiro <ryan.monteiro@aluno.ufes.br>
2018-11-24 11:54:10 -0200

    aprendendo input

---
+++ learning free_code_camp/input.py
@@ -0,0 +1,10 @@
+# name = input("Enter your name: ")
+# age = input("Enter your age: ")
+# print("Hello " + name.capitalize() + "! You are " + age + " vezes corno" )
+
+num1 =  input("Enter a number: ")
+num2 = input("Enter another number: ")
+# result = int(num1) + int(num2)
+result = float(num1) + float(num2)
+
+print(result)
